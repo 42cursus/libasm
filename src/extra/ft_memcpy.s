a92aa0908c58e64d9206077507850fa1fa395288
Andrei Belov <abelov@student.42london.com>
2026-06-03 20:56:56 +0100

    extra: rewrite ft_memcpy.s as a hand-written NASM implementation
    The previous src/extra/ft_memcpy.s was the compiler's output for a C
    memcpy: lots of .L2/.L4/.L6 labels, branchy 8/4/2/1-byte fast paths,
    and no project header.
    
    Replace it with a hand-written NASM version following the same style
    as the rest of the project:
      * standard 42 header, .rodata/.bss/.text.pad/.text section layout
      * proper frame, callee-saved register discipline, .end size label
      * NULL-pointer guard on either dst or src returns NULL (defensive;
        libc memcpy is undefined here, but ft_memcpy lives in src/extra/
        so it does not need to mirror libc exactly)
      * straight byte loop, easy to step through in GDB / objdump
    
    ft_memcpy is not part of the libasm subject. It stays under src/extra/
    and is therefore not linked into libasm.a.

--- src/extra/ft_memcpy.s
+++ src/extra/ft_memcpy.s
@@ -1,37 +1,65 @@
+; ************************************************************************** ;
+;                                                                            ;
+;                                                        :::      ::::::::   ;
+;   ft_memcpy.s                                        :+:      :+:    :+:   ;
+;                                                    +:+ +:+         +:+     ;
+;   By: abelov <abelov@student.42london.com>       +#+  +:+       +#+        ;
+;                                                +#+#+#+#+#+   +#+           ;
+;   Created: 2026/06/02 11:50:00 by abelov            #+#    #+#             ;
+;   Updated: 2026/06/02 11:50:00 by abelov           ###   ########.fr       ;
+;                                                                            ;
+; ************************************************************************** ;
+
+bits 64
+default rel
+
+%define save_pointer    qword[rbp - 8] ; QWORD PTR -8[rbp]
+
+SECTION .rodata
+SECTION .bss
+
+section .text.pad exec nowrite align=1
+    nop
+
+global  ft_memcpy:function (ft_memcpy.end - ft_memcpy)
+
+SECTION .text
+
+; void	*ft_memcpy(void *dest, const void *src, size_t n)
 ft_memcpy:
-	mov	rax, rdi
-	cmp	rdx, 8
-	jnb	.L2
-	test	dl, 4
-	je	.L4
-	mov	ecx, dword [rsi]
-	mov	dword [rdi], ecx
-	mov	ecx, dword -4[rsi+rdx]
-	mov	dword -4[rdi+rdx], ecx
-	ret
-.L4:
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 16
+	mov	save_pointer, rdi
+
+	test	rsi, rsi
+	sete	al
+	test	rdi, rdi
+	sete	cl
+	or	al, cl
+	je	.loop_start
+	mov	save_pointer, 0
+	jmp	.loop_end
+
+.loop_start:
 	test	rdx, rdx
-	je	.L3
-	mov	cl, byte [rsi]
-	mov	byte [rdi], cl
-	test	dl, 2
-	je	.L3
-	mov	cx, word -2[rsi+rdx]
-	mov	word -2[rdi+rdx], cx
+	je	.loop_end
+
+.loop_body:
+	mov	rcx, rsi
+	add	rsi, 1
+	mov	rax, rdi
+	add	rdi, 1
+	movzx	ecx, byte [rcx]
+	mov	byte [rax], cl
+
+.loop_iter:
+	sub	rdx, 1
+	jne	.loop_body
+
+.loop_end:
+	mov	rax, save_pointer
+	leave
 	ret
-.L2:
-	mov	rcx, qword -8[rsi+rdx]
-	mov	qword -8[rdi+rdx], rcx
-	dec	rdx
-	cmp	rdx, 8
-	jb	.L3
-	and	rdx, -8
-	xor	ecx, ecx
-.L6:
-	mov	rdi, qword [rsi+rcx]
-	mov	qword [rax+rcx], rdi
-	add	rcx, 8
-	cmp	rcx, rdx
-	jb	.L6
-.L3:
-	ret
+
+.end:
