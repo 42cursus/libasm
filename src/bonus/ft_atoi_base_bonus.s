e3aa005ec074fed2b6be51ec0d3304f600e10315
Andrei Belov <abelov@student.42london.com>
2026-09-08 01:27:16 +0100

    fix: widen atoi base arithmetic
    Signed-off-by: Andrei Belov <abelov@student.42london.com>

--- src/bonus/ft_atoi_base_bonus.s
+++ src/bonus/ft_atoi_base_bonus.s
@@ -15,34 +15,30 @@ default rel
 
 SECTION .rodata
 SECTION .bss
-section .text.pad exec nowrite align=1
-	nop
+
+SECTION .text exec nowrite align=16 ; Section containing code
 
 global	ft_atoi_base:function (ft_atoi_base.end - ft_atoi_base)
-global	ft_atoi_base.validate_base:function (ft_atoi_base.validate_base_end - ft_atoi_base.validate_base)
-global	ft_atoi_base.skip_whitespace:function (ft_atoi_base.skip_whitespace_end - ft_atoi_base.skip_whitespace)
-global	ft_atoi_base.parse_digits:function (ft_atoi_base.parse_digits_end - ft_atoi_base.parse_digits)
-global	ft_atoi_base.invalid:function (ft_atoi_base.invalid_end - ft_atoi_base.invalid)
-global	ft_atoi_base.done:function (ft_atoi_base.end - ft_atoi_base.done)
+global	ft_atoi_base.validate_base:object hidden (ft_atoi_base.validate_base_end - ft_atoi_base.validate_base)
+global	ft_atoi_base.skip_whitespace:object hidden (ft_atoi_base.consume_signs - ft_atoi_base.skip_whitespace)
+global	ft_atoi_base.parse_digits:object hidden (ft_atoi_base.apply_sign - ft_atoi_base.parse_digits)
+global	ft_atoi_base.invalid:object hidden (ft_atoi_base.done - ft_atoi_base.invalid)
+global	ft_atoi_base.done:object hidden (ft_atoi_base.end - ft_atoi_base.done)
 
-BASE_MIN_LENGTH	equ 2
-ASCII_TAB		equ 9
+BASE_MIN_LENGTH         equ 2
+ASCII_TAB		        equ 9
 ASCII_CARRIAGE_RETURN	equ 13
-ASCII_SPACE		equ 32
+ASCII_SPACE		        equ 32
 
 ; ft_atoi_base register roles
 %define reg_string_cursor	rdi
 %define reg_base_arg		rsi
-%define reg_result		eax
+%define reg_result		    rax
 %define reg_character		dl
 %define reg_base_length		rcx
-%define reg_base_length_dword	ecx
-%define reg_sign		r8d
+%define reg_sign		    r8d
 %define reg_base_start		r9
 %define reg_base_index		r10
-%define reg_base_index_dword	r10d
-
-SECTION .text
 
 ; int ft_atoi_base(char *str, char *base);
 ft_atoi_base:
@@ -70,6 +66,7 @@ ft_atoi_base:
 
 .duplicate_scan_start:
 	lea	reg_base_index, [reg_base_length + 1]
+
 .duplicate_scan:
 	cmp	byte [reg_base_start + reg_base_index], 0
 	je	.duplicate_scan_end
@@ -77,9 +74,11 @@ ft_atoi_base:
 	je	.invalid
 	inc	reg_base_index
 	jmp	.duplicate_scan
+
 .duplicate_scan_end:
 	inc	reg_base_length
 	jmp	.validate_base
+
 .validate_base_end:
 	cmp	reg_base_length, BASE_MIN_LENGTH
 	jb	.invalid
@@ -90,13 +89,13 @@ ft_atoi_base:
 	cmp	reg_character, ASCII_SPACE
 	je	.advance_whitespace
 	cmp	reg_character, ASCII_TAB
-	jb	.skip_whitespace_end
+	jb	.consume_signs
 	cmp	reg_character, ASCII_CARRIAGE_RETURN
-	ja	.skip_whitespace_end
+	ja	.consume_signs
+
 .advance_whitespace:
 	inc	reg_string_cursor
 	jmp	.skip_whitespace
-.skip_whitespace_end:
 
 .consume_signs:
 	cmp	reg_character, '+'
@@ -104,6 +103,7 @@ ft_atoi_base:
 	cmp	reg_character, '-'
 	jne	.parse_digits
 	neg	reg_sign
+
 .advance_sign:
 	inc	reg_string_cursor
 	mov	reg_character, [reg_string_cursor]
@@ -111,24 +111,26 @@ ft_atoi_base:
 
 .parse_digits:
 	xor	reg_result, reg_result
+
 .next_digit:
 	mov	reg_character, [reg_string_cursor]
 	test	reg_character, reg_character
 	je	.apply_sign
 	xor	reg_base_index, reg_base_index
-.find_digit:
+
+.find_digit_loop:
 	cmp	reg_character, [reg_base_start + reg_base_index]
 	je	.digit_found
-	cmp	byte [reg_base_start + reg_base_index], 0
+	cmp	BYTE [reg_base_start + reg_base_index], 0
 	je	.apply_sign
 	inc	reg_base_index
-	jmp	.find_digit
+	jmp	.find_digit_loop
+
 .digit_found:
-	imul	reg_result, reg_base_length_dword
-	add	reg_result, reg_base_index_dword
+	imul	reg_result, reg_base_length
+	add	reg_result, reg_base_index
 	inc	reg_string_cursor
 	jmp	.next_digit
-.parse_digits_end:
 
 .apply_sign:
 	cmp	reg_sign, 1
@@ -138,8 +140,9 @@ ft_atoi_base:
 
 .invalid:
 	xor	reg_result, reg_result
-.invalid_end:
 
 .done:
 	ret
+
 .end:
+    nop
